8384a090c45f983aaa7297b06c8e50a0ff4731d4
vkreddy317 <vijaychinna0808@gmail.com>
2024-11-23 20:20:24 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # End-to-end-object-detection
+
+# How to run:
+
+```bash
+conda create --name signlang python=3.7
+```
+
+```bash
+conda activate signlang
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="signLanguages",
+    version="0.0.0",
+    author="Vijay Kumar Reddy P",
+    author_email="vijaychinna0808@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
